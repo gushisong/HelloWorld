67bbb21f085903835982a20b720a83c6ccc1e440
xiaolei.song <xiaolei.song@oracle.com>
2018-07-05 16:02:57 +0800

    A

---
+++ src/main/java/com/oracle/peoplesoft/bass2/qe/utility/Constants.java
@@ -0,0 +1,25 @@
+package com.oracle.peoplesoft.bass2.qe.utility;
+
+public class Constants {
+	
+	public static final String BASS2_URL="http://scliawd.us.oracle.com:8200/psp/EMHD8SPQ/?cmd=login";
+	
+	public static final String BugAU_URL="https://bugau.oraclecorp.com/index.html";
+	
+	public static final String URL="http://toolsqa.wpengine.com/automation-practice-form";
+	
+	public static final String BaseBug="27988933";
+	
+	public static final String BugAu_UserName="xiaolei.song@oracle.com";
+	
+	public static final String BugAu_Password="IRL-local-1234";
+	
+	public static final String UserName="PSQA";
+	
+	public static final String Password="QA";
+	
+	public static final String BrowserName="Chrome";
+	
+	public static final int WaitForTimeOut=20; 
+
+}
